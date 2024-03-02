4f5e77684b151636e721c2839d4f4e95dd74d64a
Lukas Andersson <120193922+LukasA-dev@users.noreply.github.com>
2024-03-02 01:46:35 +0100

    Improved readability from  weather response message

--- src/commands/weather.js
+++ src/commands/weather.js
@@ -1,5 +1,6 @@
 // Import dependencies
 const axios = require('axios');
+const { roundToTenth } = require('../utilities/roundToTenth');
 
 // Mapping of weather conditions to emojis
 const weatherEmojis = {
@@ -30,8 +31,10 @@ module.exports = {
       const { data } = response;
       const weatherCondition = data.weather[0].main;
       const emoji = weatherEmojis[weatherCondition] || '';
+      const roundedTemp = roundToTenth(data.main.temp);
+      const roundedFeelsLike = roundToTenth(data.main.feels_like);
 
-      const weatherInfo = `The current weather in ${data.name} is ${data.weather[0].description} ${emoji} with a temperature of :thermometer:${data.main.temp}°C.`;
+      const weatherInfo = `The current weather in ${data.name} is ${data.weather[0].description} ${emoji} with a temperature of ${roundedTemp}°C :thermometer: and feels like ${roundedFeelsLike}°C :thermometer:.`;
       message.reply(weatherInfo);
     } catch (error) {
       console.error('Weather command error:', error);
---
+++ src/utilities/roundToTenth.js
@@ -0,0 +1,6 @@
+// Function to round a number to the nearest tenth
+function roundToTenth(temp) {
+    return Math.round(temp * 10) / 10;
+}
+
+module.exports = { roundToTenth };
