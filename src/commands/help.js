9b0bd22ec946c395836f38d907c976dd7f81e8cd
Lukas Andersson <120193922+LukasA-dev@users.noreply.github.com>
2024-03-11 15:53:45 +0100

    Now answers in an embedded message

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,21 +1,33 @@
+const { EmbedBuilder } = require("discord.js");
+
 module.exports = {
   name: "help",
   description: "Provides help on commands.",
   detailedDescription:
     "Use `k!help [command]` to get detailed information on a specific command. If no command is specified, a list of all available commands will be displayed.",
   execute(message, args, client) {
+    const helpEmbed = new EmbedBuilder()
+      .setColor("#0099ff") // Set the color of the embed
+      .setTitle("Help") // Set the title of the embed
+      .setFooter({
+        text: "Use k!help [command] for more details on a specific command.",
+      }) // Add a footer
+      .setTimestamp(); // Add the current timestamp
+
     // If no command is specified, list all available commands
     if (args.length === 0) {
-      let helpMessage = "Available commands:\n";
+      let commandsList = "";
 
-      // Append each command and its description to the help message
+      // Append each command and its description to the commandsList string
       client.commands.forEach((command) => {
-        helpMessage += `- \`k!${command.name}\`: ${command.description}\n`;
+        commandsList += `- \`k!${command.name}\`: ${command.description}\n`;
       });
 
-      helpMessage +=
-        "Use: `k!help [command]` for detailed information on a specific command.";
-      message.channel.send(helpMessage);
+      helpEmbed
+        .setDescription("Available commands:")
+        .addFields({ name: "Commands", value: commandsList });
+
+      message.channel.send({ embeds: [helpEmbed] });
     } else {
       // If a specific command is requested, display detailed information about it
       const commandName = args[0].toLowerCase();
@@ -23,12 +35,19 @@ module.exports = {
       if (client.commands.has(commandName)) {
         const command = client.commands.get(commandName);
         const description = command.detailedDescription || command.description; // Use detailedDescription if available
-        message.channel.send(`\`k!${command.name}\`: ${description}`);
+
+        helpEmbed
+          .setTitle(`Help: k!${command.name}`)
+          .setDescription(description);
+
+        message.channel.send({ embeds: [helpEmbed] });
       } else {
         // Handle unknown command requests
-        message.channel.send(
-          "Unknown command. Try `k!help` for a list of all commands."
-        );
+        helpEmbed
+          .setTitle("Unknown Command")
+          .setDescription("Try `k!help` for a list of all commands.");
+
+        message.channel.send({ embeds: [helpEmbed] });
       }
     }
   },
